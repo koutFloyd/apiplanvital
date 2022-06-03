f483091d3c4a738db6e2e40793a255c065145b4f
OTORRES <ostorresm@gmail.com>
2022-06-02 22:23:06 -0400

    commit t

--- heroku.rb
+++ heroku.rb
@@ -1,6 +1,6 @@
 
-system("git commit -m \"commit message link dissable\"")
-
+#system("git commit -m \"commit message link dissable\"")
+#system("git commit -a")
 #system("db:migrate")
 system("heroku login")
 #system("horoku create")
