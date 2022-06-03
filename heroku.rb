303a4f397a990a0b985675b49084be381eee725c
OTORRES <ostorresm@gmail.com>
2022-06-02 22:00:43 -0400

    commit x

--- heroku.rb
+++ heroku.rb
@@ -1,7 +1,8 @@
 
-system("bundle install")
+system("git commit -m \"commit message link dissable\"")
+
 #system("db:migrate")
-#system("heroku login")
+system("heroku login")
 #system("horoku create")
-#system("git push heroku master")
+system("git push heroku master")
 #system("heroku run rake db:migrate")
